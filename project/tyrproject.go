dfd2bec05b9f394ce68cce2cc144410dcf8ce6f2
kuso19970916 <kuso19970916@hotmail.com.tw>
2021-01-15 10:25:01 +0800

    create setconfig

--- project/tyrproject.go
+++ project/tyrproject.go
@@ -20,13 +20,15 @@ var (
 	Interface string = "interface"
 	//Usecase -
 	Usecase string = "usecase"
+	//Config -
+	Config string = "config"
 )
 
 //SetProject -
 func SetProject() {
 	FName = getFName()
 	setapp()
-	//setconfig()
+	setconfig()
 }
 
 func getFName() string {
@@ -48,7 +50,22 @@ func setapp() {
 }
 
 func setconfig() {
-	os.Mkdir("config", os.ModePerm)
+	os.Mkdir(Config, os.ModePerm)
+
+	Content := []byte("package config")
+	config := fmt.Sprintf("%s/%s", Config, "config.go")
+	err := ioutil.WriteFile(config, Content, os.ModePerm)
+	if err != nil {
+		fmt.Println("ioutil WriteFile error: ", err)
+	}
+	Data := fmt.Sprintf("%s%s","package config\nvar (\n// ConsulHost -\nConsulHost string\n// APP -\nAPP = \n// VERSION -\nVERSION string\n// BUILD -\nBUILD string\n// COMMIT -\nCOMMIT string\n// PORT - server port\nPORT string\n",		"// ZONE - time zone\nZONE = time.FixedZone(, 8*60*60)\n// ISUSEIP - IP\nISUSEIP bool\n)\nvar (\n// C - \nC = &Config{}\n)\n// Config - 配置\ntype Config struct {\n	}\n//api -\nconst (\n)\n ")
+	envContent := []byte(Data)
+
+	env := fmt.Sprintf("%s/%s", Config, "env.go")
+	err = ioutil.WriteFile(env, envContent, os.ModePerm)
+	if err != nil {
+		fmt.Println("ioutil WriteFile error: ", err)
+	}
 }
 
 func setdomain() {
@@ -57,14 +74,28 @@ func setdomain() {
 	os.MkdirAll(Path, os.ModePerm)
 	Model := fmt.Sprintf("%s/%s", Path, "model")
 	os.MkdirAll(Model, os.ModePerm)
+
 	Repository := fmt.Sprintf("%s/%s", Path, "repository")
 	os.MkdirAll(Repository, os.ModePerm)
+	Data := []byte("package repository")
+	Repo := fmt.Sprintf("%s/%s%s", Path, FName, "_repository.go")
+	err := ioutil.WriteFile(Repo, Data, os.ModePerm)
+	if err != nil {
+		fmt.Println("ioutil WriteFile error: ", err)
+	}
+
 	Service := fmt.Sprintf("%s/%s", Path, "service")
 	os.MkdirAll(Service, os.ModePerm)
+	content := []byte("package service")
+	Interface := fmt.Sprintf("%s/%s", Path, "interface.go")
+	err = ioutil.WriteFile(Interface, content, os.ModePerm)
+	if err != nil {
+		fmt.Println("ioutil WriteFile error: ", err)
+	}
 }
 
 func setinfra() {
-	//建立資料夾
+	//取得建立的路徑
 	Path := fmt.Sprintf("%s/%s", App, Infra)
 	os.MkdirAll(Path, os.ModePerm)
 	Config := fmt.Sprintf("%s/%s", Path, "config")
@@ -90,7 +121,6 @@ func setusecase() {
 	Interface := fmt.Sprintf("%s/%s", Path, "interface.go")
 	Usecase := fmt.Sprintf("%s/%s%s", Path, FName, "_usecase.go")
 	UsecaseTest := fmt.Sprintf("%s/%s%s", Path, FName, "_test.go")
-	fmt.Println(FName)
 	//將指定內容寫入到檔案中
 	err := ioutil.WriteFile(Interface, content, os.ModePerm)
 	if err != nil {
